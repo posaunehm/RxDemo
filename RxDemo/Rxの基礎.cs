3eca5936a935abf6974dc58c6c90b020f892dcd1
posaunehm <posaunehm@gmail.com>
2014-09-22 08:13:15 +0900

    First Commit

---
+++ RxDemo/Rxの基礎.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using NUnit.Framework;
+using System.Reactive.Linq;
+
+namespace RxDemo
+{
+    [TestFixture]
+    public class Rxの基礎
+    {
+        [TestCase]
+        public void まずはLinqのおさらい()
+        {
+            var rand = new Random();
+
+            var DateTimeのリスト =
+                (from _ in Enumerable.Range(0, 100)
+                    select new DateTime(
+                        (long) (rand.NextDouble()*DateTime.MaxValue.Ticks)))
+                        .ToList();
+
+            var 紀元1000年以後 = 
+                from dt in DateTimeのリスト
+                where dt.Year >= 1000
+                select dt;
+
+            var 閏年のみ = 
+                from dt in DateTimeのリスト
+                where dt.Year % 4 == 0
+                select dt;
+
+            var 隣り合う二つの差が1000年以上 =
+                from dt in DateTimeのリスト
+                select new {First = dt, Second = dt};
+
+            紀元1000年以後.All(time => time.Year > 1000).IsTrue();
+            閏年のみ.All(time => time.Year % 4  == 0).IsTrue();
+            隣り合う二つの差が1000年以上.All(pair => Math.Abs( pair.First.Year - pair.Second.Year )> 1000).IsTrue();
+        }
+
+        [TestCase]
+        public void 最初のRx()
+        {
+            //Rx : Reactive Extentions
+            
+
+            var Int型のObservable = Observable.Range(0, 100).Publish();
+
+            Int型のObservable
+                .Where(x => x%3 == 0)
+                .Subscribe(_ => Debug.WriteLine("Fizz"));
+            Int型のObservable
+                .Where(x => x % 5 == 0)
+                .Subscribe(_ => Debug.WriteLine("Buzz"));
+            Int型のObservable
+                .Where(x => x % 5 != 0 && x % 3 != 0)
+                .Subscribe(num => Debug.WriteLine(num));
+
+
+            Int型のObservable.Connect();
+
+        }
+
+
+    }
+}
---
+++ RxDemoForms/Drawer.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Drawing;
+using System.Reactive.Linq;
+using System.Windows.Forms;
+
+namespace RxDemoForms
+{
+    public partial class Drawer : Form
+    {
+        public Drawer()
+        {
+            InitializeComponent();
+
+            RegisterRxEvents();
+        }
+
+
+
+
+        #region 出来上がり_イベント駆動
+
+        ////--- ドラッグ中のフラグと前の点を覚えるプロパティ
+        //private bool IsDragging { get; set; }
+        //private Point PreviousLocation { get; set; }
+
+        //protected override void OnMouseDown(MouseEventArgs e)
+        //{
+        //    //--- 開始点を記憶
+        //    this.PreviousLocation = e.Location;
+        //    base.OnMouseDown(e);
+        //}
+
+        //protected override void OnMouseMove(MouseEventArgs e)
+        //{
+        //    //--- 左ボタンが押されていればドラッグ開始
+        //    if (!this.IsDragging)
+        //        if (e.Button.HasFlag(MouseButtons.Left))
+        //            this.IsDragging = true;
+
+        //    //--- ドラッグ中は前の点と今の点を結ぶ
+        //    if (this.IsDragging)
+        //    {
+        //        using (var graphic = this.CreateGraphics())
+        //        using (var pen = new Pen(Color.Red, 1))
+        //            graphic.DrawLine(pen, this.PreviousLocation, e.Location);
+        //        this.PreviousLocation = e.Location; //--- 今の点を覚えておく
+        //    }
+        //    base.OnMouseMove(e);
+        //}
+
+        //protected override void OnMouseUp(MouseEventArgs e)
+        //{
+        //    //--- ドラッグ状態をクリア
+        //    this.IsDragging = false;
+        //    this.PreviousLocation = Point.Empty;
+        //    base.OnMouseUp(e);
+        //}
+
+        #endregion
+
+        #region 出来上がり_Rx
+
+        private void RegisterRxEvents()
+        {
+            //--- ドラッグ処理を規定
+            IObservable<MouseEventArgs> drag = this.MouseDownAsObservable()
+                        .SelectMany(_ => this.MouseMoveAsObservable())
+                        .TakeUntil(this.MouseUpAsObservable());
+            drag.Zip //--- 前後の値でパッケージ化
+            (
+                drag.Skip(1),
+                (x, y) => new { Prev = x.Location, Next = y.Location }
+            )
+            .Repeat() //--- 1ストロークで終わらず、何度も繰り返す
+            .Subscribe(location =>
+            {
+                //--- 前の点と次の点を直線で結ぶ
+                using (var graphic = this.CreateGraphics())
+                using (var pen = new Pen(Color.Red, 1))
+                    graphic.DrawLine(pen, location.Prev, location.Next);
+            });
+        }
+
+
+
+        IObservable<MouseEventArgs> MouseDownAsObservable()
+        {
+            return Observable.FromEvent<MouseEventHandler, MouseEventArgs>
+            (
+                handler => (sender, e) => handler(e),
+                handler => this.MouseDown += handler,
+                handler => this.MouseDown -= handler
+            );
+        }
+
+        IObservable<MouseEventArgs> MouseMoveAsObservable()
+        {
+            return Observable.FromEvent<MouseEventHandler, MouseEventArgs>
+            (
+                handler => (sender, e) => handler(e),
+                handler => this.MouseMove += handler,
+                handler => this.MouseMove -= handler
+            );
+        }
+
+        IObservable<MouseEventArgs> MouseUpAsObservable()
+        {
+            return Observable.FromEvent<MouseEventHandler, MouseEventArgs>
+            (
+                handler => (sender, e) => handler(e),
+                handler => this.MouseUp += handler,
+                handler => this.MouseUp -= handler
+            );
+        }
+
+        #endregion
+    }
+}
